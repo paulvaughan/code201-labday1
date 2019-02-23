89cf1c248aac274ffa06476737a86503968398b3
Pablo <paulvaughan.dev@gmail.com>
2019-02-23 10:04:31 -0500

    created variable to hold the p tag from my index.html file. p tag will display the user's entry answers

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let yourName = prompt ('What is your name?')
 let favoriteSport = prompt ('What is your favorite sport?')
 let favoriteRelax = prompt ('What is your favorite thing to do to relax?')
-confirm(`${yourName} your favorite sport is ${favoriteSport} and to relax you usually ${favoriteRelax}`)
-console.log(`${yourName} your favorite sport is ${favoriteSport} and to relax you usually ${favoriteRelax}`)
+// Create a variable to hold the p tag from our index.html fil
+let para = document.getElementById('para')
+console.log(para)
+//Set the innerText of the p tag using the user's answers
+para.innerText =`${yourName} your favorite sport is ${favoriteSport} and to relax you usually ${favoriteRelax}`
+//confirm(`${yourName} your favorite sport is ${favoriteSport} and to relax you usually ${favoriteRelax}`)
+//console.log(`${yourName} your favorite sport is ${favoriteSport} and to relax you usually ${favoriteRelax}`)
